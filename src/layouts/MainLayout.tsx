87c964f16c7b1bc87313a93092431a0a57dfe4c1
Jaime-GEB <jaimegomez@inade.es>
2026-04-09 12:36:31 +0200

    enhancement: deleted unused files

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,10 +3,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <main className="h-screen w-full flex flex-col overflow-hidden">
-       <section className="w-full flex-1 min-h-0 flex flex-col">
+        <section className="w-full flex-1 min-h-0 flex flex-col">
           <ThemeToggle />
             {children}
-       </section>
+        </section>
     </main>
   );
 };
